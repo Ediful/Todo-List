2c3f661528b7c5e9a5b8e8e6c175795d16dd43cb
Ediful <56943939+Ediful@users.noreply.github.com>
2021-09-14 13:22:12 -0500

    project class

---
+++ src/project.js
@@ -0,0 +1,10 @@
+// Project class
+// Projects have a single title and one or more todos
+// There is a default Tasks project that is initilized at the start
+// More projects can be created by user
+
+class Project {
+    constructor(name) {
+      this.name = name;
+    }
+}
