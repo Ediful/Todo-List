e51493acbdb2d20667efc79ca397da31dfa0340a
Ediful <56943939+Ediful@users.noreply.github.com>
2021-09-21 15:52:22 -0500

    working on localstorage

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ let Tasks = new Project("Tasks");
 
 Tasks.addNewTodo("First Todo");
 
+localStorage.setItem("Tasks", JSON.stringify(Tasks));
+
+console.log(localStorage.getItem("Tasks"));
+
 console.log(Tasks.getTodos());
