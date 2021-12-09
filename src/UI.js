95dddfb9167b98f328f494f33456e66fd8ead045
Ediful <56943939+Ediful@users.noreply.github.com>
2021-12-09 14:37:25 -0600

    focusing on UI

--- src/UI.js
+++ src/UI.js
@@ -1,41 +1,66 @@
 import { Project } from "./project";
 
 let Projects = [];
+let activeProject = null;
 
 export default class UI {
+  // Loading initial content
+  static loadHomepage() {
+    UI.loadNewProjectBtns();
+    UI.loadNewTaskBtns();
+  }
 
-    // Loading initial content
-    static loadHomepage() {
-        UI.loadNewProjectBtns();
-    }
+  // creates behavior for all the project related buttons
+  static loadNewProjectBtns() {
+    let newProjectForm = document.getElementById("new-project-form");
+    let newProjectBtn = document.getElementById("new-project-btn");
+    let newProjectCloseBtn = document.getElementById("new-project-close");
+    let newProjectSubmitBtn = document.getElementById("new-project-submit");
 
-    // creates behavior for all the project related buttons
-    static loadNewProjectBtns() {
-        let newProjectForm = document.getElementById("new-project-form");
-        let newProjectBtn = document.getElementById("new-project-btn");
-        let newProjectCloseBtn = document.getElementById("new-project-close");
-        let newProjectSubmitBtn = document.getElementById("new-project-submit");
+    newProjectBtn.addEventListener("click", () => (newProjectForm.style.display = "block"));
+    newProjectCloseBtn.addEventListener("click", () => (newProjectForm.style.display = "none"));
+    newProjectSubmitBtn.addEventListener("click", () => UI.submitNewProject());
+  }
 
-        newProjectBtn.addEventListener("click", () => newProjectForm.style.display = "block");
-        newProjectCloseBtn.addEventListener("click", () => newProjectForm.style.display = "none");
-        newProjectSubmitBtn.addEventListener("click", () => UI.submitNewProject());
-    }
+  // adds new Project object to the Projects array and then calls displayNewProject
+  static submitNewProject() {
+    let newProjectTitle = document.getElementById("new-project-title").value;
+    let newProject = new Project(newProjectTitle);
+    Projects.push(newProject);
+    UI.displayNewProject(newProject);
+  }
 
-    // adds new Project object to the Projects array and then calls displayNewProject
-    static submitNewProject() {
-        let newProjectTitle = document.getElementById("new-project-title").value;
-        let newProject = new Project(newProjectTitle);
-        Projects.push(newProject);
-        UI.displayNewProject(newProject);
-    }
+  // add new project to the Projects list on the sidebar
+  static displayNewProject(newProject) {
+    let projectList = document.getElementById("project-list");
 
-    // add new project to the Projects list on the sidebar
-    static displayNewProject(newProject) {
-        let projectList = document.getElementById("project-list");
+    const newProjectEntry = document.createElement("h3");
 
-        const newProjectEntry = document.createElement('h3');
+    newProjectEntry.textContent = newProject.title;
+    projectList.appendChild(newProjectEntry);
+  }
 
-        newProjectEntry.textContent = newProject.title;
-        projectList.appendChild(newProjectEntry);
-    }
+  // creates behavior for all task related buttons
+  static loadNewTaskBtns() {
+    let newTaskForm = document.getElementById("new-task-form");
+    let newTaskBtn = document.getElementById("new-task-btn");
+    let newTaskCloseBtn = document.getElementById("new-task-close");
+    let newTaskSubmitBtn = document.getElementById("new-task-submit");
+
+    newTaskBtn.addEventListener("click",() => (newTaskForm.style.display = "block"));
+    newTaskCloseBtn.addEventListener("click",() => (newTaskForm.style.display = "none"));
+    newTaskSubmitBtn.addEventListener("click", () => UI.submitNewTask());
+  }
+
+  // adds new Task object to the current project and then calls displayNewTask
+  static submitNewTask() {
+    let newTaskTitle = document.getElementById("new-task-title").value;
+    let newTaskDueDate = document.getElementById("new-task-due-date").value;
+    let newTaskDescription = document.getElementById("new-task-description").value;
+
+    // need to track active project, should be kept here. Not needed anywhere else
+    // create new task object
+    
+    // add task to active project
+  }
 }
